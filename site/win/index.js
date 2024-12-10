c28dc00123dd56d174aa8c607ee5f00f35abc8b2
teotoivo <103033915+teotoivo@users.noreply.github.com>
2024-12-10 19:29:58 +0200

    shit

--- site/win/index.js
+++ site/win/index.js
@@ -6,6 +6,14 @@ async function main() {
     const distance = urlParams.get('distance');
     const name = urlParams.get('name');
 
+    const co2Element = document.querySelector('.co2');
+    const distanceElement = document.querySelector('.distance');
+    const nameElement = document.querySelector('.name');
+
+    co2Element.innerText = `You used ${co2} kg of CO2`;
+    distanceElement.innerText = `You traveled ${distance} km`;
+    nameElement.innerText = `Hello ${name}!`;
+
 }
 
 main()
